6e26a5c1d0d3cc8391245c4b32bbc6df51967497
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-11 16:00:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -205,7 +205,7 @@ async function generateHTML(domains, SITENAME) {
         </div>
       </div>
       <div class="footer">
-        Powered by yutian81 | <a href="https://github.com/yutian81/domain-check">Fork for Github</a>
+        Powered by yutian81 | <a href="https://github.com/yutian81/domain-check">Fork from Github</a>
       </div>
     </body>
     </html>
